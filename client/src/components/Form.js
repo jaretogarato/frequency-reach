7fe588e527fecbce7e6a02ba87026c6715c2aba3
Jared Gallardo <jaredgg@gmail.com>
2017-09-27 04:16:35 -0600

    updated form with semantic ui

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { updateStats } from '../actions/stats';
+import { Form } from 'semantic-ui-react';
 
 // page_views impressions unique_visitors
 
-class Form extends React.Component {
+class InputForm extends React.Component {
   state = { pageViews: 5000000, uniqueVisitors: 800000, impressions: 1100000 }
 
   onChange = (e) => {
@@ -36,27 +37,60 @@ class Form extends React.Component {
 
   render() {
     return (
+      // <div>
+      //   <h1>Site and Advertiser Data</h1>
+      //   <form onSubmit={this.handleSubmit}>
+      //     <h3><strong>Site Statistics:</strong></h3>
+      //     <span>Page Views: <input
+      //       value={this.state.pageViews}
+      //       placeholder={5000000}
+      //       name="pageViews"
+      //       onChange={this.onChange} /></span><br />
+      //     <span>Unique Visitors: <input
+      //       value={this.state.uniqueVisitors}
+      //       placeholder={800000}
+      //       name="uniqueVisitors"
+      //       onChange={this.onChange} /></span><br />
+      //     <span>Impressions: <input
+      //       value={this.state.impressions}
+      //       placeholder={1000000}
+      //       name="impressions"
+      //       onChange={this.onChange} /></span><br />
+      //   </form>
+      //   <div>&nbsp;</div>
+      // </div>
       <div>
-        <h1>Input Form</h1>
-        <form onSubmit={this.handleSubmit}>
-          <h3><strong>Site Statistics:</strong></h3>
-          <span>Page Views: <input
-            value={this.state.pageViews}
-            placeholder={5000000}
-            name="pageViews"
-            onChange={this.onChange} /></span><br />
-          <span>Unique Visitors: <input
-            value={this.state.uniqueVisitors}
-            placeholder={800000}
-            name="uniqueVisitors"
-            onChange={this.onChange} /></span><br />
-          <span>Impressions: <input
-            value={this.state.impressions}
-            placeholder={1000000}
-            name="impressions"
-            onChange={this.onChange} /></span><br />
-        </form>
-        <div>&nbsp;</div>
+        <h1>Site and Advertiser Data</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Monthly Page Views</label>
+            <input
+              value={this.state.pageViews}
+              placeholder={this.state.pageViews}
+              name="pageViews"
+              onChange={this.onChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Monthly Unique Visitors</label>
+            <input
+              value={this.state.uniqueVisitors}
+              placeholder={800000}
+              name="uniqueVisitors"
+              onChange={this.onChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Monthly Unique Visitors</label>
+            <input
+              value={this.state.impressions}
+              placeholder={1000000}
+              name="impressions"
+              onChange={this.onChange}
+            />
+          </Form.Field>
+
+        </Form>
       </div>
     )
   }
@@ -70,4 +104,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Form);
+export default connect(mapStateToProps)(InputForm);
